36d9dc01a9e73b4a1eb3f42309cc387c5e9b6a12
Jonathan Boone <jb@moonshineinteractive.com>
2014-07-10 12:53:41 -0500

    changed base url

--- config/staging.js
+++ config/staging.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base_uri: 'https://ec2-54-200-41-62.us-west-2.compute.amazonaws.com',
+  base_uri: 'https://prizm-fe-dev-926980177.us-west-2.elb.amazonaws.com',
   app_name: 'Prizm Staging',
   mongo: {
     host: 'internal-Prizm-DB-DEV-LB-1375164738.us-west-2.elb.amazonaws.com',
