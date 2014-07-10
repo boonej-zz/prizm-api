efd6991c265979431ad783bcca12a4d620800da5
Jonathan Boone <jb@moonshineinteractive.com>
2014-07-10 14:27:07 -0500

    fixed new relic... again

--- newrelic.js
+++ newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name : process.env.NEW_RELIC_NAME || config.app_name,
+  app_name : config.app_name,
   /**
    * Your New Relic license key.
    */
