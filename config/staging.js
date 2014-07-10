c5fac2a5ed08860ede8311c1e957a6acdd25f6b1
Jonathan Boone <jb@moonshineinteractive.com>
2014-07-10 12:01:12 -0500

    modified config

--- config/staging.js
+++ config/staging.js
@@ -1,4 +1,8 @@
 module.exports = {
   base_uri: 'https://ec2-54-200-41-62.us-west-2.compute.amazonaws.com',
-  app_name: 'Prizm Staging'
+  app_name: 'Prizm Staging',
+  mongo: {
+    host: 'internal-Prizm-DB-DEV-LB-1375164738.us-west-2.elb.amazonaws.com',
+    name: 'prism'
+  }
 };
