32c21e7f3c9d8d8afef8f28a3a60b933c311369a
Jonathan Boone <jb@moonshineinteractive.com>
2014-07-22 18:49:50 -0500

    added production config

---
+++ config/production.js
@@ -0,0 +1,8 @@
+module.exports = {
+  base_uri: 'PRIZM-FE-PRD-1163268161.us-west-2.elb.amazonaws.com',
+  app_name: 'Prizm Production',
+  mongo: {
+    host: 'internal-PrizmProductionDB-588525252.us-west-2.elb.amazonaws.com',
+    name: 'prizm'
+  }
+};
