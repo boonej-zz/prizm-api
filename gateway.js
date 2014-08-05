000618c171db20b33fea135dc3090663ae089404
Jonathan Boone <jb@higheraltitude.com>
2014-08-04 22:53:43 -0500

    disabling auth

--- gateway.js
+++ gateway.js
@@ -44,6 +44,7 @@ module.exports = function(req, res, next){
  */
 var needsAuthorization = function(req){
   var path = _utils.requestPathArray(req);
+  console.log(path[0]);
   if (path[0] == 'posts') {
     return false;
   }
