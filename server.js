8fc3556118923ed19eb245de6e5e731cba646c12
Jonathan Boone <jb@higheraltitude.com>
2014-08-04 22:01:28 -0500

    redirect changes

--- server.js
+++ server.js
@@ -300,7 +300,7 @@ _https.createServer(ssl_options, _app).listen(_config.env.port, function(){
   }
 });
 */
-_app.all('*', function(req, res, next){
+_app.get('*', function(req, res, next){
   if (req.headers['x-forwarded-proto'] != 'https') {
     res.redirect('https://' + req.host + req.originalUrl);
   } else {
