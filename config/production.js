b178eb89bb285c0b40a029fa0563aefd56e8968b
Jonathan Boone <jb@higheraltitude.com>
2014-08-05 17:34:10 -0500

    remove auth

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   base_uri: 'PRIZM-FE-PRD-1163268161.us-west-2.elb.amazonaws.com',
   app_name: 'Prizm Production',
   mongo: {
-    host: 'prizm:vuxar6facUth@internal-PrizmProductionDB-588525252.us-west-2.elb.amazonaws.com',
+    host: 'internal-PrizmProductionDB-588525252.us-west-2.elb.amazonaws.com',
     name: 'prizm'
   }
 };
