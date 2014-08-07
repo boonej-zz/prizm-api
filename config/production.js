66ebe7cecfc4865e1393787d932f88cefa23811a
Jonathan Boone <jb@higheraltitude.com>
2014-08-07 09:46:34 -0500

    made config changes

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   base_uri: 'PRIZM-FE-PRD-1163268161.us-west-2.elb.amazonaws.com',
   app_name: 'Prizm Production',
   mongo: {
-    host: 'internal-PrizmProductionDB-588525252.us-west-2.elb.amazonaws.com',
+    host: 'prizm:vuxar6facUth@54.191.239.14',
     name: 'prizm'
   }
 };
