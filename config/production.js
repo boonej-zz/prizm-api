11e973df12354854566f52e568867ba1d8546b7e
Jonathan Boone <jb@higheraltitude.com>
2014-08-19 23:56:54 -0500

    reverting

--- config/production.js
+++ config/production.js
@@ -2,7 +2,7 @@ module.exports = {
   base_uri: 'PRIZM-FE-PRD-1163268161.us-west-2.elb.amazonaws.com',
   app_name: 'Prizm Production',
   mongo: {
-    host: 'prizm:vuxar6facUth@54.213.111.108',
+    host: 'prizm:vuxar6facUth@54.191.239.14',
     name: 'prizm'
   },
   social: {
