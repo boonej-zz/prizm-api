2ee8785715b89b99de764cbb571c705cb82b44bd
Jonathan Boone <jb@higheraltitude.com>
2014-10-07 22:02:20 -0500

    added missing files

---
+++ models/insight.js
@@ -0,0 +1,126 @@
+/**
+ * Insight Model
+ * Jonathan Boone
+ */
+
+var mongoose     = require('mongoose'),
+    utils        = require(process.env.PRISM_HOME + 'utils'),
+    User          = require(process.env.PRISM_HOME + 'models/user').User,
+    Trust         = require(process.env.PRISM_HOME + 'models/user').Trust,
+    Post          = require(process.env.PRISM_HOME + 'models/post').Post,
+    ObjectId    = mongoose.Schema.Types.ObjectId;
+
+var insightSchema = new mongoose.Schema({
+  creator         : {type: ObjectId, ref: 'User', required: true},
+  create_date     : {type: Date, default: null, required: false, index: true},
+  title           : {type: String, default: null, required: true},
+  text            : {type: String, default: null, required: true}, 
+  file_path       : {type: String, default: ''},
+  likes_count     : {type: Number, default: 0},
+  dislikes_count  : {type: Number, default: 0},
+  tags_count      : {type: Number, default: 0},
+  tags            : {type: Array, default: []},
+  hash_tags       : {type: [String], default: []},
+  hash_tags_count : {type: Number, default: 0},
+  link            : {type: String, default: null},
+  link_title      : {type: String, default: null}
+}); 
+
+insightSchema.statics.selectFields = function(type){
+  var select = ['_id', 'text', 'creator','create_date','likes_count',
+      'dislikes_count', 'tags_count', 'tags', 'hash_tags', 'hash_tags_count',
+      'file_path', 'title', 'link', 'link_title'];
+  return select;
+};
+
+insightSchema.methods.format = function(type, add_fields){
+  var format;
+  if(!type) type = 'basic';
+
+  format = {
+    _id:                  this._id,
+    creator:                 this.text,
+    create_date:          this.create_date,
+    file_path:            this.file_path,
+    likes_count:          this.likes_count,
+    dislikes_count:       this.dislikes_count,
+    hash_tags_count:      this.hash_tags_count,
+    hash_tags:            this.hash_tags,
+    tags:                 this.tags,
+    title:                this.title,
+    tags_count:           this.tags_count,
+    link:                 this.link,
+    link_title:           this.link_title
+  };
+
+  if(add_fields){
+    if(typeof add_fields === 'string') format[add_fields] = this[add_fields];
+    if(Array.isArray(add_fields) && add_fields.length > 0){
+      for(var idx in add_fields){
+        format[add_fields[idx]] = this[add_fields[idx]];
+      }
+    }
+  }
+  return format;
+};
+var insightTargetSchema = new mongoose.Schema({
+  create_date     : {type: Date, default: null, required: false, index: true},
+  insight         : {type: ObjectId, ref: 'Insight', required: true},
+  creator         : {type: ObjectId, ref: 'User', required: true},
+  target          : {type: ObjectId, ref: 'User', required: true},
+  liked           : {type: Boolean, default: false},
+  disliked        : {type: Boolean, default: false}
+});
+
+insightSchema.pre('save', function(next){
+  this.create_date = Date.now();
+  next();
+});
+
+insightTargetSchema.pre('save', function(next){
+  this.create_date = Date.now();
+  next();
+});
+
+insightTargetSchema.statics.selectFields = function(type){
+  var select = ['_id', 'create_date', 'insight','target','liked',
+      'disliked', 'creator'];
+  return select;
+};
+
+insightTargetSchema.statics.canResolve = function(){
+  return [
+    {target: {identifier: '_id', model: 'User'}},
+    {insight: {identifier: '_id', model: 'Insight'}},
+    {creator: {identifier: '_id', model: 'User'}}
+  ];
+};
+
+
+insightSchema.methods.format = function(type, add_fields){
+  var format;
+  if(!type) type = 'basic';
+
+  format = {
+    _id:                  this._id,
+    target:               this.target,
+    create_date:          this.create_date,
+    insight:              this.insight,
+    liked:                this.liked,
+    disliked:             this.disliked,
+    creator:              this.creator
+  };
+
+  if(add_fields){
+    if(typeof add_fields === 'string') format[add_fields] = this[add_fields];
+    if(Array.isArray(add_fields) && add_fields.length > 0){
+      for(var idx in add_fields){
+        format[add_fields[idx]] = this[add_fields[idx]];
+      }
+    }
+  }
+  return format;
+};
+
+mongoose.model('Insight', insightSchema);
+mongoose.model('InsightTarget', insightTargetSchema);
---
+++ routes/insights.js
@@ -0,0 +1,200 @@
+var mongoose   = require('mongoose'),
+    prism_home = process.env.PRISM_HOME,
+    utils      = require(prism_home + 'utils'),
+    logger     = require(prism_home + 'logs.js'),
+    PrismError  = require(prism_home + 'error'),
+    User        = mongoose.model('User'),
+    Twine       = require(prism_home + 'classes/Twine'),
+    Insight     = mongoose.model('Insight'),
+    InsightTarget = mongoose.model('InsightTarget'),
+    _ = require('underscore'),
+    ObjectId    = mongoose.Schema.Types.ObjectId;
+
+var ServerError = function(res){
+  utils.prismResponse(res, null, false, PrismError.serverError);
+};
+
+exports.createInsight = function(req, res){
+  console.log('creating insight');
+  console.log(req.body);
+  if (req.body.creator){
+    console.log('found creator');
+    User.findOne(req.body.creator, function(err, user){
+      if (err) {
+        console.log(err);
+        utils.prismResponse(res, null, false, PrismError.invalidRequest);
+      }
+      var insight = new Insight({
+        creator: user._id,
+        create_date: Date.now(),
+        text: req.body.text,
+        title: req.body.title
+      });
+      if (req.body.file_path && req.body.file_path != 'undefined'){
+        insight.file_path = req.body.file_path;
+      }
+      if (req.body.text && req.body.text != 'undefined'){
+        insight.file_path = req.body.file_path;
+      }
+      if (req.body.link && req.body.link != 'undefined'){
+        insight.link = req.body.link;
+      }
+      if (req.body.link_title && req.body.link_title != 'undefined'){
+        insight.link_title = req.body.link_title;
+      }
+      insight.save(function(error, insight){
+        if(error){
+          console.log(error);
+          utils.prismResponse(res, null, false, PrismError.invalidUserRequest);
+        } 
+        res.send('success');
+
+      });
+    });
+  } else {
+    utils.prismResponse(res, null, false, PrismError.invalidUserRequest);
+  }
+};
+
+exports.fetchInsights = function(req, res) {
+  console.log('fetching insights');
+    criteria = {};
+  new Twine('Insight', criteria, req, null, function(error, result){
+    if (error) {
+      logger.log('error', 'Fetching insights returned error: ', {err: error});
+      ServerError(res);
+    } else {
+      utils.prismResponse(res, result, true);
+    }
+  });   
+};
+
+exports.fetchUserInsights = function(req, res) {
+  console.log('fetching user insights');
+  var uid = req.params.id;
+  var $res = res;
+  User.findOne({_id: uid}, function(err, user){
+    if (err) {
+      res.send('error');
+    } else {
+      console.log('found user');
+      var criteria = {target: user._id};
+      new Twine('InsightTarget', criteria, req, null, function(error, result){
+        if (error) {
+          console.log(error);
+          logger.log('error', 'Fetching user insights returned error', {err: error});
+          ServerError(res);
+        } else {
+          console.log('found ' + result.data.length + ' results');
+          utils.prismResponse($res, result, true);
+        }
+      }); 
+    }
+
+  });
+};
+
+exports.sendInsight = function(req, res) {
+  console.log('sending insight');
+  console.log(req.body);
+  var insightId = req.params.id;
+  var userList = req.body.users;
+  var uid = req.body.uid;
+  if (insightId && uid){
+    console.log('searching for insight');
+    var criteria = {_id: insightId};
+    console.log(uid);
+    Insight.findOne(criteria, function(err, insight){
+      if (err) {
+        console.log(err);
+      } else {
+        User.findOne({_id: uid}, function(err, user){
+          if (err){
+            console.log(err);
+          } else {
+            console.log('found user');
+            var it = new InsightTarget({
+              target: user._id,
+              insight: insight._id,
+              creator: insight.creator
+            });
+            it.save(function(err, result){
+              if (err) {
+                console.log('there was a problem');
+              } else {
+                console.log('target saved');
+              }
+            });
+          }
+        });
+      }
+    });
+    res.send('done');
+  }
+};
+
+exports.likeInsight = function(req, res){
+  if (req.params.id){
+    console.log('liking insight');
+    var it_id = req.params.id;
+    InsightTarget.findOne({_id: it_id})
+    .populate('insight')
+    .exec(function(err, it){
+      if (err) {
+        console.log(err);
+        res.send(error);
+      } else {
+        console.log('liking insight');
+        console.log(it);
+        if (! it.liked) {
+          it.liked = true;
+          it.insight.likes_count = it.insight.likes_count + 1;
+          if (it.disliked){
+            it.disliked = false;
+            it.insight.dislikes_count -= 1;
+          }
+          console.log(it);
+          it.save(function(err, result){
+            console.log('saved');
+            it.insight.save(); 
+            res.send('success');
+          });
+        } else {
+          res.send('already like');
+        }
+      }
+    });
+  } else {
+    res.send('error');
+  }
+};
+
+exports.dislikeInsight = function(req, res){
+  if (req.params.id){
+    console.log('disliking insight');
+    var it_id = req.params.id;
+    InsightTarget.findOne({_id: it_id})
+      .populate('insight')
+      .exec(function (err, it){
+        if (! it.disliked) {
+          it.disliked = true;
+          it.insight.dislikes_count = it.insight.dislikes_count + 1;
+          if (it.liked){
+            it.liked = false;
+            it.insight.likes_count -= 1;
+          }
+          it.save(function(err, result){
+            console.log('saved');
+            it.insight.save();
+            res.send('success');
+          });
+        } else {
+          res.send('already disliked');
+        }
+      });
+  } else {
+    res.send('error');
+  }
+}
+
+
