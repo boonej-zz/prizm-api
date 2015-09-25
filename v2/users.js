94d3b62de49abc129acba7c36f4e1d430311f208
Jonathan Boone <jb@moonshineinteractive.com>
2015-09-25 13:32:15 -0500

    added groups and orgs to user search

--- v2/users.js
+++ v2/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId;
 var config = require('config');
 var gateway = require('../gateway');
 var _ = require('underscore');
@@ -9,11 +10,20 @@ var User = mongoose.model('User');
 
 app.get('/', function(req, res){
   var searchText = req.query.search || false;
+  var organization = req.query.organization || false;
+  var group = req.query.group || false;
   var limit = req.params.limit || 5;
   var params = {};
   if (searchText) {
     var r = new RegExp(searchText, 'i');
     params.name = r;
+  }
+  if (organization) {
+    var orgStatus = {organization: ObjectId(organization), status: 'active'};
+    if (group) {
+      orgStatus.group = ObjectId(group);
+    }
+    params.org_status = {$elemMatch: orgStatus};
   }
   console.log(params);
   User.findBasic(params, limit, function(err, users){
