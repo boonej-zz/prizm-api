56981801899948b7d4a65f1ebe3b732c3bc76ac5
Jonathan Boone <jb@moonshineinteractive.com>
2015-10-02 12:32:50 -0500

    fix

--- models/user.js
+++ models/user.js
@@ -732,8 +732,8 @@ userSchema.statics.addToGroup = function(uid, group, next){
     console.log(user.name + ': ' + user.org_status.length);
     if (user) {
       _.each(user.org_status, function(o){
-        console.log(o._id + ':' + o.organization);
-        if (String(o._id) == String(o.organization)) {
+        console.log(group.organization + ':' + o.organization);
+        if (String(group.organization) == String(o.organization)) {
           var exists = false;
           _.each(o.groups, function(g){
             if (String(g) == String(group._id)){
