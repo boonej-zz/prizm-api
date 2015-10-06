5dc2089eef505a345ca37bc4eadeac9b785b7692
Jonathan Boone <jb@moonshineinteractive.com>
2015-10-06 16:52:11 -0500

    m

--- v2/organizations.js
+++ v2/organizations.js
@@ -7,7 +7,7 @@ var Organization = mongoose.model('Organization');
 var User = mongoose.model('User');
 var Message = mongoose.model('Message');
 var Group = mongoose.model('Group');
-var ObjectId = require('mongoose').Types.ObjectId;
+var ObjectId = mongoose.Types.ObjectId;
 var _ = require('underscore');
 
 // Organization Endpoints
@@ -136,8 +136,8 @@ app.put('/:oid/groups/:gid', function(req, res) {
         console.log(gid);
         User.find({active: true, org_status: {
           $elemMatch: {
-            organization: oid,
-            groups: gid 
+            organization: ObjectId(oid),
+            groups: ObjectId(gid) 
           }
         }
         }, function(err, users){
