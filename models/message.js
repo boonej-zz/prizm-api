6cb8614136033e9da903c2ea79bc7cb8ff0142bb
Jonathan Boone <jb@moonshineinteractive.com>
2015-10-23 10:23:29 -0500

    fixed bug

--- models/message.js
+++ models/message.js
@@ -187,7 +187,7 @@ var notifyUsers = function(message){
   var params = {};
   var body = "";
   var model = mongoose.model('Message');
-  m.prettyText(function(prettyText){
+  message.prettyText(function(prettyText){
     model.populate(message, {path: 'group', model: 'Group'} ,function(err, m){
       model.populate(m, {path: 'creator', model: 'User', select: {name: 1}}, function(err, m){
         model.populate(m, {path: 'target', model: 'User', select: {first_name: 1, google_devices: 1, device_token: 1, badge_count: 1}}, function(err, m){
