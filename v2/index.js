9f732efa800dccaf5f921b8d18d7edb5d2d38269
Jonathan Boone <jb@moonshineinteractive.com>
2015-10-29 10:10:10 -0500

    added interests

--- v2/index.js
+++ v2/index.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var organizations = require('./organizations');
 var users = require('./users');
+var interests = require('./interests');
 
 app.use('/organizations/', organizations); 
 app.use('/users/', users);
+app.use('/interests/', interests);
 app.get('/', function(req, res){
   res.send('Welcome to the V2 API');
 });
