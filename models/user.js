1679fd8ec163ab41fc555d9e97c3d01e2f6407ab
Jonathan Boone <jb@moonshineinteractive.com>
2015-11-02 12:17:21 -0600

    modify

--- models/user.js
+++ models/user.js
@@ -209,6 +209,7 @@ userSchema.statics.findOneCore = function(uid, next) {
           user.primary_organization = org._id;
           user.theme = org.theme.background_url;
           user.role = 'owner';
+          user.date_founded = user.date_founded.toString();
         }
         next(err, user);
       });
