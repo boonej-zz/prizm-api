cc8af321595431c4c8564aab1ddbadde3cee98e6
Jonathan Boone <jb@moonshineinteractive.com>
2015-11-17 11:01:35 -0600

    added posts endpoint

--- v2/index.js
+++ v2/index.js
@@ -3,10 +3,12 @@ var app = express();
 var organizations = require('./organizations');
 var users = require('./users');
 var interests = require('./interests');
+var posts = require('./posts');
 
 app.use('/organizations/', organizations); 
 app.use('/users/', users);
 app.use('/interests/', interests);
+app.use('/posts/', posts);
 app.get('/', function(req, res){
   res.send('Welcome to the V2 API');
 });
